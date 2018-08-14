d2f2c51892c0ef51377d47593ffae991b2b5f661
zhaoxiaobo <m18725917011@163.com>
2018-08-14 22:43:38 +0800

    merge

--- fatherzbb/son1/src/main/java/com/son1/App.java
+++ fatherzbb/son1/src/main/java/com/son1/App.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class App {
-    public static void main( String[] args ){
+    public static void main( String[] args ){//son1
     	SpringApplication.run(App.class, args);
     }
 }
